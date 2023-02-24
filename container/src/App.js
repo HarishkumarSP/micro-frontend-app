b3ac1415a8b4e686731b56a7f5544ec5ea1a478e
harishkumarsp <harishkumarsp1998@gmail.com>
2023-02-24 15:21:13 +0530

    styles issues fixed

--- container/src/App.js
+++ container/src/App.js
@@ -1,16 +1,25 @@
 import React from "react";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 import { BrowserRouter } from "react-router-dom";
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 const App = () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
 
